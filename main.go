1c0a800c6d0cb92b06aa3024c4d4f02584cef418
Bikash01293 <adhikaribikash01293@gmail.com>
2022-03-15 15:12:51 +0530

    update and delete is ready to update and delete the product

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -20,7 +20,7 @@ type Products struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	ProductName string             `json:"productname,omitempty" bson:"productname,omitempty"`
 }
-
+//Db connection
 func ConnectDb() *mongo.Collection {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
@@ -40,7 +40,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("Bad Request !")
 	}
 }
-
+//Getting all the products
 func GetHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	var products []Products
@@ -73,6 +73,7 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Creating a new product according to the client request.
 func CreateProduct(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -95,10 +96,40 @@ func Update(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+//Updating the product according to the client request.
 func UpdateProduct(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("GET params were:", r.URL.Query())
-	params := r.URL.Query()
-	json.NewEncoder(w).Encode(params)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	id := r.URL.Query().Get("id")
+	fmt.Println("id =>", id)
+
+	ids, _ := primitive.ObjectIDFromHex(id)
+
+	var product Products
+
+	filter := bson.M{"_id": ids}
+
+	_ = json.NewDecoder(r.Body).Decode(&product)
+
+	update := bson.D{
+		{"$set", bson.D{
+			{"productname", product.ProductName},
+		}},
+	}
+
+	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&product)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	product.ID = ids
+
+	json.NewEncoder(w).Encode(product)
+
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
@@ -110,24 +141,64 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Deleting the product according to the client request.
 func DeleteProduct(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("GET params were:", r.URL.Query())
-	params := r.URL.Query()
+
+	w.Header().Set("Content-Type", "application/json")
 	id := r.URL.Query().Get("id")
 	fmt.Println("id =>", id)
-	json.NewEncoder(w).Encode(params)
+
+	ids, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal(err, w)
+	}
+	fmt.Println(ids)
+
+	filter := bson.M{"_id": ids}
+
+	
+
+	var products Products
+	cur, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(context.TODO())
+
+	for cur.Next(context.TODO()) {
+
+		var product Products
+		err := cur.Decode(&product)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		products = product
+	}
+
+
+	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+
+	if err != nil {
+		log.Fatal(err, w)
+	}
+
+	fmt.Println(deleteResult)
+
+	w.WriteHeader(http.StatusAccepted)
+
+	json.NewEncoder(w).Encode(products)
 }
 
 func main() {
 	ConnectDb()
-	http.HandleFunc("/home", Home)
+	http.HandleFunc("/products", Home)
 	http.HandleFunc("/create", Create)
-	http.HandleFunc("/update/{id}", Update)
-	http.HandleFunc("/delete/{id}", Delete)
+	http.HandleFunc("/update", Update)
+	http.HandleFunc("/delete", Delete)
 
 	fmt.Println("starting server...")
 	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
-
